2a34abea2e0f8eeddc3f810c37659924ac007990
MartinLaursen <111317@rts-365.dk>
2018-01-11 09:42:35 +0100

    opgave 11
    Opgave 11 er færdig nu.

--- opgave_11_simple_gallery_with_array/myscript.js
+++ opgave_11_simple_gallery_with_array/myscript.js
@@ -4,23 +4,32 @@ var filnavne = [
     "img/cd3.jpg"
 ];
 var i = 0;
-console.log(filnavne);
+var pic = document.getElementById("myPic");
 document.addEventListener("DOMContentLoaded", event => {
     function opdater_billede() {
-        var pic = document.getElementById("myPic");
         pic.setAttribute('src', filnavne[i]); 
     };
     function naeste_billede () {
-        document.querySelector('button').addEventListener('click', () =>{
-            filnavne[i];
-            i = i++;
-
-            // var count = filnavne.length;
-            // for (let i = 0; i < count; i++) {
-            // }
+        document.querySelector('#next').addEventListener('click', () =>{
+            // i++ til slut af if skal være øverst før det virker, ellers giver den fejl på sidste billede
+            i++;
+            if (i > 2) {
+                i = 0;
+            }
+            pic.setAttribute('src', filnavne[i]);
+        });
+    }
+    function forige_billede () {
+        document.querySelector('#previous').addEventListener('click', () =>{
+            i--;
+            if (i < 0) {
+                i = 2;
+            }
+            pic.setAttribute('src', filnavne[i]);
         });
     }
     opdater_billede();
     naeste_billede();
+    forige_billede();
     
 });
